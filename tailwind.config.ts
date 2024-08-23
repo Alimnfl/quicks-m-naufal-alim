1235b3b9a72796f446bb6c8d676d1b4d2dbd6714
Alimnfl <mnaufallim234@gmail.com>
2024-08-23 15:02:23 +0700

    Updated custom color for styling tailwind colors.

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,20 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      colors: {
+        primary: {
+          1: "#2F80ED", // Blue
+          2: "#4F4F4F", // Dark Gray
+          3: "#828282", // Medium Gray
+          4: "#E0E0E0", // Light Gray
+        },
+        indicator: {
+          1: "#F8B76B", // Orange
+          2: "#8785FF", // Light Purple
+          3: "#EB5757", // Red
+          4: "#F2C94C", // Yellow
+        },
+      },
     },
   },
   plugins: [],
