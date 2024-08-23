4ddc6c76148d9759f00f6014c0ec6cbb4d9daa2b
Alimnfl <mnaufallim234@gmail.com>
2024-08-24 00:51:22 +0700

    Initialize and solve inbox/task.

---
+++ app/task/layout.tsx
@@ -0,0 +1,13 @@
+import React, { ReactNode } from "react";
+import Navbar from "../(content)/_components/Navbar";
+
+function Layout({ children }: { children: ReactNode }) {
+  return (
+    <div className="flex flex-col w-full h-full">
+      <Navbar />
+      {children}
+    </div>
+  );
+}
+
+export default Layout;
---
+++ app/task/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import QuickNav from "../(content)/_components/QuickNav";
+
+function Page() {
+  return (
+    <div className="relative flex flex-col max-w-2xl mx-auto w-full h-full">
+      <QuickNav />
+    </div>
+  );
+}
+
+export default Page;
