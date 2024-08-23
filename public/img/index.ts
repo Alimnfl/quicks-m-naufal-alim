7c7f23c7fab8d54b9b9e357f0b3c42f725ca2a73
Alimnfl <mnaufallim234@gmail.com>
2024-08-23 22:42:40 +0700

    Updated image

--- public/img/index.ts
+++ public/img/index.ts
@@ -3,5 +3,13 @@ import InboxTrigger from "@/public/img/inbox-trigger.png";
 import TaskActive from "@/public/img/task-active.png";
 import TaskTrigger from "@/public/img/task-trigger.png";
 import QuicksTrigger from "@/public/img/quicks-trigger.png";
+import GroupDiscussion from "@/public/img/group-discussion.png";
 
-export { InboxActive, InboxTrigger, TaskActive, TaskTrigger, QuicksTrigger };
+export {
+  InboxActive,
+  InboxTrigger,
+  TaskActive,
+  TaskTrigger,
+  QuicksTrigger,
+  GroupDiscussion,
+};
