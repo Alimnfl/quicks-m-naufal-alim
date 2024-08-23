e4cae312f1e6ec5bc1ff5f7c1188f91d4eae3311
Alimnfl <mnaufallim234@gmail.com>
2024-08-23 15:01:42 +0700

    Initialize image for component

---
+++ public/img/index.ts
@@ -0,0 +1,7 @@
+import InboxActive from "@/public/img/inbox-active.png";
+import InboxTrigger from "@/public/img/inbox-trigger.png";
+import TaskActive from "@/public/img/task-active.png";
+import TaskTrigger from "@/public/img/task-trigger.png";
+import QuicksTrigger from "@/public/img/quicks-trigger.png";
+
+export { InboxActive, InboxTrigger, TaskActive, TaskTrigger, QuicksTrigger };
