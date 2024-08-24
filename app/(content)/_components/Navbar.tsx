a2b2bbbd9287231d18de943c57ff465d1eee75f0
Alimnfl <mnaufallim234@gmail.com>
2024-08-24 10:34:09 +0700

    Updated QuickNav for pathName next/navigation

--- app/(content)/_components/Navbar.tsx
+++ app/(content)/_components/Navbar.tsx
@@ -3,7 +3,7 @@ import { BiSearch } from "react-icons/bi";
 
 function Navbar() {
   return (
-    <div className="bg-primary-2">
+    <div className="bg-primary-2 relative text-white w-full h-full">
       <div className="flex flex-row w-full h-full py-4 items-center mx-auto max-w-2xl justify-between">
         <BiSearch />
       </div>
--- app/(content)/_components/QuickNav.tsx
+++ app/(content)/_components/QuickNav.tsx
@@ -5,20 +5,19 @@ import Image from "next/image";
 import React, { useEffect, useState } from "react";
 import { DataIcon, DataIconInbox, DataIconTask } from "../_data/ContentData";
 import { AnimatePresence, motion } from "framer-motion";
+import { usePathname } from "next/navigation";
 
 function QuickNav() {
-  const [activeRoute, setActiveRoute] = useState("");
   const [triggerQuick, setTriggerQuick] = useState<boolean>(false);
+  const pathname = usePathname();
 
   const toggleTrigger = () => {
     setTriggerQuick((prev) => !prev);
   };
 
-  useEffect(() => {
-    if (typeof window !== "undefined") {
-      setActiveRoute(window.location.pathname);
-    }
-  }, []);
+  const isLanding = pathname === "/";
+  const isInbox = pathname.startsWith("/inbox");
+  const isTask = pathname.startsWith("/task");
 
   const containerVariant = {
     hidden: {
@@ -60,102 +59,104 @@ function QuickNav() {
   };
 
   return (
-    <div className="w-full h-full flex flex-row justify-end">
-      {activeRoute === "/" && (
-        <AnimatePresence>
-          {triggerQuick && (
-            <motion.div
-              variants={containerVariant}
-              initial="hidden"
-              animate="show"
-              exit="exit"
-              className="flex gap-2 flex-row"
-            >
-              {DataIcon.map((d) => (
-                <motion.a
-                  href={d.link}
-                  key={d.id}
-                  variants={childVariant}
-                  className="flex items-center flex-col w-fit h-full"
-                >
-                  <p>{d.name}</p>
-                  <Image src={d.icon} width={68} height={68} alt="Quick" />
-                </motion.a>
-              ))}
-            </motion.div>
-          )}
-          <button
-            onClick={toggleTrigger}
-            className="w-fit h-fit ml-2 flex-col pt-6"
-          >
-            <Image src={QuicksTrigger} width={68} height={68} alt="Quick" />
-          </button>
-        </AnimatePresence>
-      )}
-
-      {activeRoute === "/inbox" && (
-        <>
+    <div className="relative w-full h-auto ">
+      <div className="fixed right-[300px] bottom-0 flex flex-row">
+        {isLanding && (
           <AnimatePresence>
-            <motion.div
-              variants={containerVariant}
-              initial="hidden"
-              animate="show"
-              exit="exit"
-              className="flex gap-2 py-4 flex-row"
+            {triggerQuick && (
+              <motion.div
+                variants={containerVariant}
+                initial="hidden"
+                animate="show"
+                exit="exit"
+                className="flex gap-2 flex-row"
+              >
+                {DataIcon.map((d) => (
+                  <motion.a
+                    href={d.link}
+                    key={d.id}
+                    variants={childVariant}
+                    className="flex items-center flex-col w-fit h-full"
+                  >
+                    <p>{d.name}</p>
+                    <Image src={d.icon} width={68} height={68} alt="Quick" />
+                  </motion.a>
+                ))}
+              </motion.div>
+            )}
+            <button
+              onClick={toggleTrigger}
+              className="w-fit h-fit ml-2 flex-col pt-6"
             >
-              {DataIconInbox.map((d) => (
-                <motion.a
-                  href={d.link}
-                  key={d.id}
-                  variants={childVariant}
-                  className="flex items-center flex-col w-full h-full"
-                >
-                  <p>{d.name}</p>
-                  <Image
-                    src={d.icon}
-                    width={68}
-                    height={68}
-                    alt="Quick"
-                    className="object-cover"
-                  />
-                </motion.a>
-              ))}
-            </motion.div>
+              <Image src={QuicksTrigger} width={68} height={68} alt="Quick" />
+            </button>
           </AnimatePresence>
-        </>
-      )}
+        )}
 
-      {activeRoute === "/task" && (
-        <>
-          <AnimatePresence>
-            <motion.div
-              variants={containerVariant}
-              initial="hidden"
-              animate="show"
-              exit="exit"
-              className="flex gap-2 py-4 flex-row"
-            >
-              {DataIconTask.map((d) => (
-                <motion.a
-                  href={d.link}
-                  key={d.id}
-                  variants={childVariant}
-                  className="flex items-center flex-col w-full h-full"
-                >
-                  <p>{d.name}</p>
-                  <Image
-                    src={d.icon}
-                    width={68}
-                    height={68}
-                    alt="Quick"
-                    className="object-cover"
-                  />
-                </motion.a>
-              ))}
-            </motion.div>
-          </AnimatePresence>
-        </>
-      )}
+        {isInbox && (
+          <>
+            <AnimatePresence>
+              <motion.div
+                variants={containerVariant}
+                initial="hidden"
+                animate="show"
+                exit="exit"
+                className="flex gap-2 py-4 flex-row"
+              >
+                {DataIconInbox.map((d) => (
+                  <motion.a
+                    href={d.link}
+                    key={d.id}
+                    variants={childVariant}
+                    className="flex items-center flex-col w-full h-full"
+                  >
+                    <p>{d.name}</p>
+                    <Image
+                      src={d.icon}
+                      width={68}
+                      height={68}
+                      alt="Quick"
+                      className="object-cover"
+                    />
+                  </motion.a>
+                ))}
+              </motion.div>
+            </AnimatePresence>
+          </>
+        )}
+
+        {isTask && (
+          <>
+            <AnimatePresence>
+              <motion.div
+                variants={containerVariant}
+                initial="hidden"
+                animate="show"
+                exit="exit"
+                className="flex gap-2 py-4 flex-row"
+              >
+                {DataIconTask.map((d) => (
+                  <motion.a
+                    href={d.link}
+                    key={d.id}
+                    variants={childVariant}
+                    className="flex items-center flex-col w-full h-full"
+                  >
+                    <p>{d.name}</p>
+                    <Image
+                      src={d.icon}
+                      width={68}
+                      height={68}
+                      alt="Quick"
+                      className="object-cover"
+                    />
+                  </motion.a>
+                ))}
+              </motion.div>
+            </AnimatePresence>
+          </>
+        )}
+      </div>
     </div>
   );
 }
