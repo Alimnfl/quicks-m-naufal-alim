29562de9bb0c7c5ff42503ff758b0486064f903e
Alimnfl <mnaufallim234@gmail.com>
2024-08-24 10:34:57 +0700

    Initialize again for QuickNav

--- app/inbox/page.tsx
+++ app/inbox/page.tsx
@@ -1,12 +1,10 @@
 import React from "react";
-import QuickNav from "../(content)/_components/QuickNav";
 import InboxContent from "./_content/InboxContent";
 
 function Page() {
   return (
     <div className="relative flex flex-col max-w-2xl mx-auto w-full h-full">
       <InboxContent />
-      <QuickNav />
     </div>
   );
 }
