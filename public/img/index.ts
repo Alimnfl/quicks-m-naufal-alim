cf1d107c59d12d4041238ed1d9eee882839a72a8
Alimnfl <mnaufallim234@gmail.com>
2024-08-25 04:22:51 +0700

    Updated for image

--- public/img/index.ts
+++ public/img/index.ts
@@ -1,15 +1,23 @@
+import CoverLogo from "@/public/img/Cover.png";
 import InboxActive from "@/public/img/inbox-active.png";
 import InboxTrigger from "@/public/img/inbox-trigger.png";
 import TaskActive from "@/public/img/task-active.png";
 import TaskTrigger from "@/public/img/task-trigger.png";
 import QuicksTrigger from "@/public/img/quicks-trigger.png";
 import GroupDiscussion from "@/public/img/group-discussion.png";
+import TimerIcon from "@/public/img/timer.svg";
+import PencilIcon from "@/public/img/pencil.svg";
+import DateIcon from "@/public/img/date.svg";
 
 export {
+  CoverLogo,
   InboxActive,
   InboxTrigger,
   TaskActive,
   TaskTrigger,
   QuicksTrigger,
   GroupDiscussion,
+  PencilIcon,
+  TimerIcon,
+  DateIcon,
 };
