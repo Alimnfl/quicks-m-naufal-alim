136b6c5746fe257291face53bb9ecf43fb537b13
Alimnfl <mnaufallim234@gmail.com>
2024-08-25 04:25:27 +0700

    Updated layouting meta data and convex provider

--- app/task/layout.tsx
+++ app/task/layout.tsx
@@ -1,6 +1,12 @@
 import React, { ReactNode } from "react";
 import Navbar from "../(content)/_components/Navbar";
 import QuickNav from "../(content)/_components/QuickNav";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Task | Quicks",
+  description: "Faster and no hassle app.",
+};
 
 function Layout({ children }: { children: ReactNode }) {
   return (
