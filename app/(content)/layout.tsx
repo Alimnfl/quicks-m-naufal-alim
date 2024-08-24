9569d2f29842d1a558768da5f2adbe0f073fd28d
Alimnfl <mnaufallim234@gmail.com>
2024-08-24 10:33:38 +0700

    Updated layouting with QuickNav (with all role)

--- app/(content)/layout.tsx
+++ app/(content)/layout.tsx
@@ -1,11 +1,13 @@
 import React, { ReactNode } from "react";
 import Navbar from "./_components/Navbar";
+import QuickNav from "./_components/QuickNav";
 
 function Layout({ children }: { children: ReactNode }) {
   return (
     <div className="flex flex-col w-full h-full">
       <Navbar />
       {children}
+      <QuickNav />
     </div>
   );
 }
