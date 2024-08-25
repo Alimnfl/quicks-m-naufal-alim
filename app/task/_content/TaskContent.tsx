22b61a15156efad0b8a08311a7050125b29ca899
Alimnfl <mnaufallim234@gmail.com>
2024-08-26 00:02:07 +0700

    Updated Content Task

--- app/task/_content/TaskContent.tsx
+++ app/task/_content/TaskContent.tsx
@@ -3,33 +3,41 @@
 import React, { ChangeEvent, useEffect, useState } from "react";
 import { AnimatePresence, motion } from "framer-motion";
 import { IoIosArrowDown, IoIosArrowUp } from "react-icons/io";
-import { DataAction, DataTask } from "../_data/DataTask";
+import { DataTask } from "../_data/DataTask";
 import {
   containerVariant,
   containerVariantDropdown,
 } from "@/app/(content)/_data/VariantMotion";
-import { PencilIcon, TimerIcon } from "@/public/img";
+import { PencilBlueIcon, PencilIcon, TimerIcon } from "@/public/img";
 import Image from "next/image";
 import { SlOptions } from "react-icons/sl";
-import { collection, query, onSnapshot } from "firebase/firestore";
+import {
+  collection,
+  query,
+  onSnapshot,
+  addDoc,
+  doc,
+  deleteDoc,
+  updateDoc,
+} from "firebase/firestore";
 import { db } from "@/app/firebase";
-// import { v4 as uuidv4 } from "uuid";
+import { v4 as uuidv4 } from "uuid";
 
 interface TaskDataProps {
   id: string;
-  date: number;
+  date: string;
   description: string;
   title: string;
   urgent: string;
+  status: boolean;
 }
 
 function TaskContent() {
   const [taskTrigger, setTaskTrigger] = useState<boolean>(false);
   const [selectedTask, setSelectedTask] = useState<string>("My Task");
   const [selectedDate, setSelectedDate] = useState<string>("");
-  const [isChecked, setIsChecked] = useState<boolean>(false);
-  const [isHidden, setIsHidden] = useState<boolean>(false);
-  const [isAction, setIsAction] = useState<boolean>(false);
+  const [isHidden, setIsHidden] = useState<{ [key: string]: boolean }>({});
+  const [isAction, setIsAction] = useState<{ [key: string]: boolean }>({});
   const [dataTask, setDataTask] = useState<TaskDataProps[]>([]);
 
   // Read items from database
@@ -46,29 +54,86 @@ function TaskContent() {
     });
   }, []);
 
+  //Deleted Item by id.
+  const deleteItem = async (id: string) => {
+    await deleteDoc(doc(db, "task-data", id));
+  };
+
+  // Save Task
+  const addTaskToFirestore = async () => {
+    const now = new Date();
+    const formattedNewDate = `${now.getFullYear()}-${String(now.getMonth() + 1)}-${String(now.getDate()).padStart(2, "0")}`;
+
+    const newTask: TaskDataProps = {
+      id: uuidv4(),
+      date: selectedDate || formattedNewDate,
+      description: "",
+      title: selectedTask || "Default Task",
+      urgent: "urgent",
+      status: false,
+    };
+
+    try {
+      await addDoc(collection(db, "task-data"), newTask);
+      setSelectedTask("My Task");
+      setSelectedDate("");
+    } catch (error) {
+      console.error("Error saving task: ", error);
+    }
+  };
+
+  // Updated task title in Database
+  const updateTask = async (
+    taskId: string,
+    updates: Partial<TaskDataProps>
+  ) => {
+    try {
+      const taskDocRef = doc(db, "task-data", taskId);
+      await updateDoc(taskDocRef, updates);
+    } catch (error) {
+      console.error("Error updating task: ", error);
+    }
+  };
+
+  const convertDate = (dateString: string): string => {
+    const [year, month, day] = dateString.split("-");
+    return `${day}/${month}/${year}`;
+  };
+
+  const convertDateForFirestore = (dateString: string): string => {
+    const [day, month, year] = dateString.split("/");
+    return `${year}-${month}-${day}`;
+  };
+
+  const handleInputChange = (
+    taskId: string,
+    field: keyof TaskDataProps,
+    value: any
+  ) => {
+    const updates: Partial<TaskDataProps> = {
+      [field]: field === "status" ? value : value,
+    };
+    updateTask(taskId, updates);
+  };
+
   const toggleTriggerTask = () => {
     setTaskTrigger((prev) => !prev);
   };
 
-  const toggleHiddenTask = () => {
-    setIsHidden((prev) => !prev);
+  const toggleHiddenTask = (id: string) => {
+    setIsHidden((prev) => ({
+      ...prev,
+      [id]: !prev[id],
+    }));
   };
 
-  const toggleHiddenAction = () => {
-    setIsAction((prev) => !prev);
+  const toggleHiddenAction = (id: string) => {
+    setIsAction((prev) => ({ ...prev, [id]: !prev[id] }));
   };
 
   const selectTask = (task: string) => {
     setSelectedTask(task);
     setTaskTrigger(false);
-  };
-
-  const handleCheckboxChange = (event: ChangeEvent<HTMLInputElement>) => {
-    setIsChecked((prev) => !prev);
-  };
-
-  const handleDateChange = (event: ChangeEvent<HTMLInputElement>) => {
-    setSelectedDate(event.target.value);
   };
 
   return (
@@ -93,7 +158,7 @@ function TaskContent() {
                 variants={containerVariantDropdown}
                 initial="hidden"
                 animate="show"
-                className="absolute top-full mt-2 ml-[84px] w-full text-primary-3 bg-white border border-gray-300 rounded-md shadow-lg"
+                className="absolute top-full z-20 mt-2 ml-[84px] w-full text-primary-3 bg-white border border-gray-300 rounded-md shadow-lg"
               >
                 {DataTask.map((d) => (
                   <div
@@ -110,6 +175,7 @@ function TaskContent() {
 
           <button
             type="button"
+            onClick={addTaskToFirestore}
             className="flex flex-row items-center text-white bg-primary-1 border-black w-fit rounded-md gap-2 p-2"
           >
             New Task
@@ -117,92 +183,138 @@ function TaskContent() {
         </div>
 
         <div className="overflow-y-auto w-full flex flex-col gap-2 h-full px-4">
-          <div className="flex flex-row gap-4 justify-between">
-            <div className="flex flex-row gap-4 items-center">
-              <div className="flex flex-col h-full w-fit pt-1">
-                <input
-                  className="w-[18px] h-[18px]"
-                  type="checkbox"
-                  checked={isChecked}
-                  onChange={handleCheckboxChange}
-                />
-              </div>
-              <div className="flex flex-col gap-2 text-primary-2">
-                <h3
-                  className={`font-bold w-[335px] text-justify h-fit ${
-                    isChecked ? "line-through text-primary-3" : ""
-                  }`}
-                >
-                  Cross-reference with Jeanne for Case #192813
-                </h3>
-                <AnimatePresence>
-                  {isHidden === false && (
-                    <motion.div
-                      variants={containerVariantDropdown}
-                      initial="hidden"
-                      animate="show"
-                      exit="exit"
-                      className="flex flex-col gap-2 overflow-hidden "
-                    >
-                      <div className="flex flex-row gap-4">
-                        <Image src={TimerIcon} alt="" />
-                        <motion.div className="">
-                          <input
-                            value={selectedDate}
-                            onChange={handleDateChange}
-                            type="date"
-                            className="border border-primary-3 rounded-md py-1 px-2"
-                          />
-                        </motion.div>
-                      </div>
-                      <div className="w-full flex flex-row gap-4 items-center">
-                        <Image src={PencilIcon} alt="" />
-                        <input
-                          spellCheck="false"
-                          name="message"
-                          className="outline-none w-full border-none resize-none px-2"
-                          placeholder="No Description"
-                        />
-                      </div>
-                    </motion.div>
-                  )}
-                </AnimatePresence>
-              </div>
-            </div>
-            <div className="items-start justify-start h-full w-fit ">
-              <div className="flex flex-row items-center gap-2">
-                <div className="font-base text-indicator-3">2 Days Left</div>
-                <div className="text-primary-2 ">12/06/2021</div>
-                <button onClick={toggleHiddenTask}>
-                  {isHidden ? <IoIosArrowUp /> : <IoIosArrowDown />}
-                </button>
-
-                <div className="relative">
-                  <button onClick={toggleHiddenAction}>
-                    <SlOptions size={12} />
-                  </button>
-                  {isAction && (
-                    <motion.div
-                      variants={containerVariantDropdown}
-                      initial="hidden"
-                      animate="show"
-                      className="absolute top-0 mt-2 ml-[84px] w-full text-primary-3 bg-white border border-gray-300 rounded-md shadow-lg"
-                    >
-                      {DataAction.map((d) => (
-                        <div
-                          key={d.id}
-                          onClick={() => selectTask(`${d.name}`)}
-                          className="px-4 py-2 cursor-pointer  hover:bg-gray-200"
-                        >
-                          {d.name}
+          {dataTask.map((data) => (
+            <div key={data.id} className="flex gap-2 justify-between flex-row">
+              <div className="flex flex-row gap-3 items-center">
+                <div className="flex flex-col h-full w-fit pt-1">
+                  <input
+                    className="w-[18px] h-[18px]"
+                    type="checkbox"
+                    checked={data.status}
+                    onChange={(e) =>
+                      handleInputChange(data.id, "status", e.target.checked)
+                    }
+                  />
+                </div>
+                <div className="flex flex-col gap-2 text-primary-2">
+                  <input
+                    value={data.title}
+                    onChange={(e) =>
+                      handleInputChange(data.id, "title", e.target.value)
+                    }
+                    onBlur={(e) =>
+                      handleInputChange(data.id, "title", e.target.value)
+                    }
+                    className={`font-bold w-[335px] outline-none text-justify h-fit ${
+                      data.status === true ? "line-through text-primary-3" : ""
+                    }`}
+                    spellCheck={false}
+                  />
+                  <AnimatePresence>
+                    {!isHidden[data.id] && (
+                      <motion.div
+                        variants={containerVariantDropdown}
+                        initial="hidden"
+                        animate="show"
+                        exit="exit"
+                        className="flex flex-col gap-2 overflow-hidden "
+                      >
+                        <div className="flex flex-row gap-4">
+                          <Image src={TimerIcon} alt="" />
+                          <motion.div className="">
+                            <input
+                              onChange={(e) =>
+                                handleInputChange(
+                                  data.id,
+                                  "date",
+                                  e.target.value
+                                )
+                              }
+                              onBlur={(e) =>
+                                handleInputChange(
+                                  data.id,
+                                  "date",
+                                  e.target.value
+                                )
+                              }
+                              value={data.date}
+                              type="date"
+                              className="border border-primary-3 rounded-md py-1 px-2"
+                            />
+                          </motion.div>
                         </div>
-                      ))}
-                    </motion.div>
-                  )}
+                        <div className="w-full relative pt-1 flex flex-row gap-4 items-start">
+                          {data.description === "" ? (
+                            <Image src={PencilIcon} alt="" className="pt-1" />
+                          ) : (
+                            <Image
+                              src={PencilBlueIcon}
+                              alt=""
+                              className="pt-1"
+                              width={16}
+                            />
+                          )}
+                          <textarea
+                            value={data.description}
+                            onChange={(e) =>
+                              handleInputChange(
+                                data.id,
+                                "description",
+                                e.target.value
+                              )
+                            }
+                            onBlur={(e) =>
+                              handleInputChange(
+                                data.id,
+                                "description",
+                                e.target.value
+                              )
+                            }
+                            spellCheck="false"
+                            name="message"
+                            className="outline-none w-[310px] resize-none overflow-hidden h-auto"
+                            placeholder="No Description"
+                          />
+                        </div>
+                      </motion.div>
+                    )}
+                  </AnimatePresence>
+                </div>
+              </div>
+              <div className="items-start justify-start h-full w-fit ">
+                <div className="flex flex-row items-center gap-2">
+                  <div className="font-base text-indicator-3">2 Days Left</div>
+                  <div className="text-primary-2 ">
+                    {convertDate(data.date)}
+                  </div>
+                  <button onClick={() => toggleHiddenTask(data.id)}>
+                    {isHidden[data.id] ? <IoIosArrowDown /> : <IoIosArrowUp />}
+                  </button>
+
+                  <div className="relative flex flex-col">
+                    <button onClick={() => toggleHiddenAction(data.id)}>
+                      <SlOptions size={12} />
+                    </button>
+                    {isAction[data.id] && (
+                      <motion.div
+                        variants={containerVariantDropdown}
+                        initial="hidden"
+                        animate="show"
+                        className="absolute z-20 top-3 right-1 mt-2 w-[] text-primary-3 bg-white border border-gray-300 rounded-md shadow-lg"
+                      >
+                        <button
+                          onClick={() => deleteItem(data.id)}
+                          className="px-2 py-2 cursor-pointer  hover:bg-gray-200"
+                        >
+                          Delete
+                        </button>
+                      </motion.div>
+                    )}
+                  </div>
                 </div>
               </div>
             </div>
-          </div>
+          ))}
         </div>
       </motion.div>
     </div>
