681507f53512cf4698bc946bfbd889782aab5a6d
Alimnfl <mnaufallim234@gmail.com>
2024-08-29 02:00:42 +0700

    Updated inbox content

--- app/inbox/_content/InboxContent.tsx
+++ app/inbox/_content/InboxContent.tsx
@@ -15,10 +15,14 @@ import {
   query,
 } from "firebase/firestore";
 import { db } from "@/app/firebase";
+import { useUser } from "@clerk/clerk-react";
+import { Spinner } from "@/app/task/_component/spinner";
+import { useConvexAuth } from "convex/react";
 
 function InboxContent() {
   const [dataInbox, setDataInbox] = useState<InboxDataProps[]>([]);
   const [isLoading, setIsLoading] = useState<boolean>(false);
+  const { isAuthenticated } = useConvexAuth();
 
   // Read items from database
   useEffect(() => {
@@ -31,15 +35,28 @@ function InboxContent() {
         const data = doc.data() as Partial<InboxDataProps>;
         itemsArr.push({ ...(data as InboxDataProps), id: doc.id });
       });
+
+      itemsArr.sort((a, b) => {
+        const dateA = new Date(`${a.date} ${b.clock}`).getTime();
+        const dateB = new Date(`${b.date} ${b.clock}`).getTime();
+        return dateB - dateA;
+      });
+
       setDataInbox(itemsArr);
       setIsLoading(false);
     });
+
+    return () => unsubscribe();
   }, []);
 
-  //Deleted Item by id.
-  const deleteItem = async (id: string) => {
-    await deleteDoc(doc(db, "task-data", id));
-  };
+  // Filter to only include unique group names
+  const uniqueDataInbox = dataInbox.reduce((acc, curr) => {
+    const x = acc.find((item) => item.groupName === curr.groupName);
+    if (!x) {
+      acc.push(curr);
+    }
+    return acc;
+  }, [] as InboxDataProps[]);
 
   return (
     <div className="flex flex-col items-end">
@@ -49,42 +66,43 @@ function InboxContent() {
         animate="show"
         className="flex flex-col gap-2 w-[634px] h-[634px] bg-white mt-10 px-10 rounded-md"
       >
-        <label className="w-full h-[40px] my-8 rounded-md items-center px-4 border border-gray-400 text-black flex flex-row">
-          <input
-            type="text"
-            placeholder="Search..."
-            className="w-full border-black"
-          />
-          <BsSearch />
-        </label>
-        <Link
-          href={`/inbox/qweqw`}
-          className="flex flex-row items-start gap-4 text-black justify-start"
-        >
-          <Image alt="" src={GroupDiscussion} width={50} height={50} />
-          <div className="flex flex-col">
-            <h2 className="font-bold text-primary-1">109220-Naturalization</h2>
-            <p className="text-sm font-medium">Camero :</p>
-            <p className="text-primary-3 text-sm">Please check this out</p>
+        {isAuthenticated && (
+          <label className="w-full h-[40px] my-8 rounded-md items-center px-4 border border-gray-400 text-black flex flex-row">
+            <input
+              type="text"
+              placeholder="Search..."
+              className="w-full border-black"
+            />
+            <BsSearch />
+          </label>
+        )}
+        {isLoading && (
+          <div className="text-center justify-center text-primary-3 pb-10 w-full h-full flex flex-col items-center gap-8">
+            <Spinner size={"icon"} />
+            Loading Inbox List
           </div>
-          <div className="text-primary-3">January 1, 2021 19:10</div>
-        </Link>
-        <div className="w-full text-primary-3 border my-4"></div>
-        {dataInbox.map((d) => (
-          <Link
-            href={`/inbox/${d.groupName}`}
-            key={d.id}
-            className="flex flex-row items-start gap-4 text-black justify-start"
-          >
-            <Image alt="" src={GroupDiscussion} width={50} height={50} />
-            <div className="flex flex-col">
-              <h2 className="font-bold text-primary-1">{d.groupName}</h2>
-              <p className="text-sm font-medium">{d.userName} :</p>
-              <p className="text-primary-3 text-sm">{d.message}</p>
-            </div>
-            <div className="text-primary-3">{d.date}</div>
-          </Link>
-        ))}
+        )}
+        {isAuthenticated &&
+          uniqueDataInbox
+            .filter(
+              (d) =>
+                d.message !== "" && d.message !== null && d.groupName !== null
+            )
+            .map((d) => (
+              <Link
+                href={`/inbox/${d.groupName}`}
+                key={d.id}
+                className="flex flex-row items-start gap-4 text-black justify-start"
+              >
+                <Image alt="" src={GroupDiscussion} width={50} height={50} />
+                <div className="flex flex-col">
+                  <h2 className="font-bold text-primary-1">{d.groupName}</h2>
+                  <p className="text-sm font-medium">{d.userName} :</p>
+                  <p className="text-primary-3 text-sm">{d.message}</p>
+                </div>
+                <div className="text-primary-3">{d.date}</div>
+              </Link>
+            ))}
       </motion.div>
     </div>
   );
